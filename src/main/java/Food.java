26fe2ae7c74b9697cc9df62482ece748428d5ccd
Aleksandergreg <alek3691@stud.kea.dk>
2023-10-02 22:02:03 +0200

    v2.0 Added Food class as a subclass from Item.

--- src/main/java/Food.java
+++ src/main/java/Food.java
@@ -1,2 +1,11 @@
-public class Food {
+public class Food extends Item{
+    private int healthPoints;
+    //make an overloaded constructor with super(itemName, itemDescription) so it inherits the variables from the Item Class
+public Food (String itemName, String itemDescription, int healthPoints){
+    super(itemName, itemDescription);
+    this.healthPoints = healthPoints;
+}
+public int getHealthPoints(){
+    return healthPoints;
+}
 }
